3fb0825a7bf8bb2d84fb38bce4bdc604df531803
Harris Marfel <hrace009@gmail.com>
2022-02-06 08:53:34 +0700

    Modify text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,8 @@
-@section('title', ' - ' . __('Dashboard'))
+@section('title', ' - ' . __('general.menu.dashboard'))
 <x-hrace009.layouts.app>
     <x-slot name="header">
         <div class="flex items-center justify-between px-4 py-4 border-b lg:py-6 dark:border-primary-darker">
-            <h1 class="text-2xl font-semibold">{{ __('Dashboard') }}</h1>
+            <h1 class="text-2xl font-semibold">{{ __('general.menu.dashboard') }}</h1>
         </div>
     </x-slot>
 
