22bcf59c09c96cff941c3147e91626590fae5a16
Harris Marfel <hrace009@gmail.com>
2022-02-06 15:52:40 +0700

    Rebuild css & script

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,3 @@
-/*
- * @author Harris Marfel <hrace009@gmail.com>
- * @link https://www.hrace009.com
- * @copyright Copyright (c) 2022.
- */
-
 //require('./bootstrap');
 import 'remixicon/fonts/remixicon.css';
 import Alpine from 'alpinejs';
