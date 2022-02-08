c6b90522a6b80b8eccad0e2490068a652095ffa3
Harris Marfel <hrace009@gmail.com>
2022-02-08 07:15:46 +0700

    fix admin view

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,16 @@
+@section('title', ' - ' . __('general.menu.dashboard'))
+<x-hrace009.layouts.admin>
+    <x-slot name="header">
+        <div class="flex items-center justify-between px-4 py-4 border-b lg:py-6 dark:border-primary-darker">
+            <h1 class="text-2xl font-semibold">{{ __('general.menu.dashboard') }}</h1>
+        </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-jet-welcome/>
+            </div>
+        </div>
+    </div>
+</x-hrace009.layouts.admin>
