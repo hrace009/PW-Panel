486dbb852d84823ef52032b624c8ccf87e5ce85b
Harris Marfel <hrace009@gmail.com>
2022-02-08 20:56:20 +0700

    create models

---
+++ app/Models/Faction.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Faction extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pwp_faction_rank';
+
+    protected $fillable = ['id', 'name', 'level', 'master', 'master_name', 'members', 'time_used', 'pk_count', 'announce', 'sys_info', 'last_op_time', 'territories', 'contribution'];
+
+    public function scopeSubType($query, $sub)
+    {
+        $column = [
+            'level' => 'level',
+            'members' => 'members',
+            'territories' => 'territories',
+            'pvp' => 'pk_count',
+        ];
+
+        return $query
+            ->whereNotIn('id', explode(',', config('pw-config.ignoreFaction')))
+            ->orderBy($column[$sub] ?? 'level', 'desc');
+    }
+}
---
+++ app/Models/Player.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Player extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pwp_player_rank';
+
+    protected $fillable = ['id', 'name', 'cls', 'gender', 'spouse', 'level', 'level2', 'hp', 'mp', 'pariah_time', 'reputation', 'time_used', 'pk_count', 'dead_flag', 'force_id', 'title_id', 'faction_id', 'faction_name', 'faction_role', 'faction_contrib', 'faction_feat', 'equipment'];
+
+    public function scopeSubType($query, $sub)
+    {
+        $column = [
+            'level' => 'level',
+            'rep' => 'reputation',
+            'time' => 'time_used',
+            'pvp' => 'pk_count'
+        ];
+
+        return $query
+            ->whereNotIn('id', explode(',', config('pw-config.ignoreRoles')))
+            ->orderBy($column[$sub] ?? 'level', 'desc');
+    }
+
+    public function getSpouse($id)
+    {
+        return $this->find($id);
+    }
+}
