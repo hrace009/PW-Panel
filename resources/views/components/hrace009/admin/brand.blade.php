628cfb8c5d65c9f2cbdec44274534136b181f818
Harris Marfel <hrace009@gmail.com>
2022-02-08 18:38:55 +0700

    create admin brand view

---
+++ resources/views/components/hrace009/admin/brand.blade.php
@@ -0,0 +1,7 @@
+<!-- Brand -->
+<a
+    href="{{ route('admin.dashboard') }}"
+    class="inline-block text-2xl font-bold tracking-wider uppercase text-primary-dark dark:text-light"
+>
+    {{ $brand }}
+</a>
