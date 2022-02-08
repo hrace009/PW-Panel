6e60cb83e059b4dc70ceb88a985820845fef93ec
Harris Marfel <hrace009@gmail.com>
2022-02-08 18:59:37 +0700

    create admin dashboard link

---
+++ app/View/Components/Hrace009/Admin/DashboardLink.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\View\Components\Hrace009\Admin;
+
+use Illuminate\View\Component;
+
+class DashboardLink extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        if (request()->routeIs('admin.dashboard')) {
+            $status = 'true';
+
+        } else {
+            $status = 'false';
+        }
+        return view('components.hrace009.admin.dashboard-link', [
+            'status' => $status,
+        ]);
+    }
+}
