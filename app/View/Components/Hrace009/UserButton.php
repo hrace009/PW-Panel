069817ba7d78275a84b3f90b0f1a0caf5fab9b92
Harris Marfel <hrace009@gmail.com>
2022-02-08 18:30:47 +0700

    create return to dashboard button (user button)

---
+++ app/View/Components/Hrace009/UserButton.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\View\Components\Hrace009;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class UserButton extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return Application|Factory|View
+     */
+    public function render()
+    {
+        return view('components.hrace009.user-button');
+    }
+}
---
+++ resources/views/components/hrace009/user-button.blade.php
@@ -0,0 +1,29 @@
+<!-- Settings button -->
+<button
+    onclick="window.location.href='{{ route('dashboard') }}'"
+    title="{{ __('general.memberArea') }}"
+    class="p-2 transition-colors duration-200 rounded-full text-primary-lighter bg-primary-50 hover:text-primary hover:bg-primary-100 dark:hover:text-light dark:hover:bg-primary-dark dark:bg-dark focus:outline-none focus:bg-primary-100 dark:focus:bg-primary-dark focus:ring-primary-darker"
+>
+    <span class="sr-only">Open admin panel</span>
+    <svg
+        class="w-7 h-7"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        aria-hidden="true"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+        />
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+        />
+    </svg>
+</button>
