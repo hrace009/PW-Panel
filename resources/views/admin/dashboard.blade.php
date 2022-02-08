02644be5b6fb7bd32206d634ecd69f0d16fcf4db
Harris Marfel <hrace009@gmail.com>
2022-02-08 20:57:24 +0700

    update dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,15 +2,11 @@
 <x-hrace009.layouts.admin>
     <x-slot name="header">
         <div class="flex items-center justify-between px-4 py-4 border-b lg:py-6 dark:border-primary-darker">
-            <h1 class="text-2xl font-semibold">{{ __('general.menu.dashboard') }}</h1>
+            <h1 class="text-2xl font-semibold">{{ __('admin.dashboard') }}</h1>
         </div>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome/>
-            </div>
-        </div>
-    </div>
+    <x-slot name="content">
+        <x-hrace009::admin.welcome/>
+    </x-slot>
 </x-hrace009.layouts.admin>
