c4e48ecb6229c1bdf60f56fbf41872bf9be7b686
Harris Marfel <hrace009@gmail.com>
2022-02-08 20:57:51 +0700

    update language admin

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -8,4 +8,9 @@
 
 return [
     'dashboard' => 'Dashboard',
+    'welcome!' => 'Welcome to your ' . config('app.name') . ' admin dashboard!',
+    'playerOnline' => 'Players Online',
+    'accountRegistered' => 'Registered Account',
+    'totalCharacter' => 'Character Created',
+    'totalFaction' => 'Faction Created',
 ];
