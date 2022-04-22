0c9c833dbfa4c93bca9a125110c6bc8d9ed3220e
Harris Marfel <hrace009@gmail.com>
2022-04-22 07:12:33 +0700

    Fix game stat

--- app/View/Components/Hrace009/Admin/GameStat.php
+++ app/View/Components/Hrace009/Admin/GameStat.php
@@ -5,7 +5,6 @@ namespace App\View\Components\Hrace009\Admin;
 use App\Models\Faction;
 use App\Models\Player;
 use App\Models\User;
-use hrace009\PerfectWorldAPI\API;
 use Illuminate\View\Component;
 
 class GameStat extends Component
@@ -27,10 +26,7 @@ class GameStat extends Component
      */
     public function render()
     {
-        $api = new API();
         return view('components.hrace009.admin.game-stat', [
-            'api' => $api,
-            'getOnlineList' => count($api->getOnlineList()),
             'totalUser' => User::count('name'),
             'totalRoles' => Player::count('name'),
             'totalFaction' => Faction::count('name'),
