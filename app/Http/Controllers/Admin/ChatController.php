3ed0d90fdc4ea370bdaf0185db449a21903c12db
Harris Marfel <hrace009@gmail.com>
2022-04-22 14:23:33 +0700

    Fix chat Controller

--- app/Http/Controllers/Admin/ChatController.php
+++ app/Http/Controllers/Admin/ChatController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Efriandika\LaravelSettings\Facades\Settings;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
