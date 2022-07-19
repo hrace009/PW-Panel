4d2e16cdfe25790b0c33514a1512201f5522b4fb
Harris Marfel <hrace009@gmail.com>
2022-07-19 08:28:44 +0700

    Add Service Link Component

---
+++ app/View/Components/Hrace009/Front/ServicesLink.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\View\Components\Hrace009\Front;
+
+use Illuminate\View\Component;
+
+class ServicesLink extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        if (request()->routeIs('app.services.index')) {
+            $status = 'true';
+        } else {
+            $status = 'false';
+        }
+        return view('components.hrace009.front.services-link', [
+            'status' => $status
+        ]);
+    }
+}
---
+++ resources/views/components/hrace009/front/services-link.blade.php
@@ -0,0 +1,30 @@
+<!-- Dashboards links -->
+<div x-data="{ isActive: {{ $status }}, open: {{ $status }} }">
+    <!-- active & hover classes 'bg-primary-100 dark:bg-primary' -->
+    <a
+        href="{{ route('app.services.index') }}"
+        class="flex items-center p-2 text-gray-500 transition-colors rounded-md dark:text-light hover:bg-primary-100 dark:hover:bg-primary"
+        :class="{'bg-primary-100 dark:bg-primary': isActive || open}"
+        role="button"
+        aria-haspopup="true"
+        :aria-expanded="(open || isActive) ? 'true' : 'false'"
+    >
+                  <span aria-hidden="true">
+                    <svg
+                        class="w-5 h-5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                      <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M14.121 10.48a1 1 0 0 0-1.414 0l-.707.706a2 2 0 1 1-2.828-2.828l5.63-5.632a6.5 6.5 0 0 1 6.377 10.568l-2.108 2.135-4.95-4.95zM3.161 4.468a6.503 6.503 0 0 1 8.009-.938L7.757 6.944a4 4 0 0 0 5.513 5.794l.144-.137 4.243 4.242-4.243 4.243a2 2 0 0 1-2.828 0L3.16 13.66a6.5 6.5 0 0 1 0-9.192z"
+                      />
+                    </svg>
+                  </span>
+        <span class="ml-2 text-sm"> {{ __('general.menu.services') }} </span>
+    </a>
+</div>
