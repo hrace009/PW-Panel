e22d6a29df8d0dafb757b9608e99ec181aeb1937
Harris Marfel <hrace009@gmail.com>
2022-08-11 21:16:26 +0700

    Create UpdateTransferCommand.php

---
+++ app/Console/Commands/UpdateTransferCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Transfer;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class UpdateTransferCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'pw:update-transfer';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update transfer table cuby';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $transfers = Transfer::all();
+        foreach ( $transfers as $transfer ){
+            if ( !DB::table( 'usecashnow' )->where( 'userid', $transfer->user_id )->where( 'zoneid', $transfer->zone_id )->take( 1 )->exists() )
+            {
+                DB::table( 'usecashnow' )->insert([
+                    'userid' => $transfer->user_id,
+                    'zoneid' => $transfer->zone_id,
+                    'sn' => 0,
+                    'aid' => 1,
+                    'point' => 0,
+                    'cash' => $transfer->cash,
+                    'status' => 1,
+                    'creatime' => Carbon::now()
+                ]);
+                DB::table( 'pwp_transfer' )->where( 'user_id', $transfer->user_id )->where( 'zone_id', $transfer->zone_id )->where( 'cash', $transfer->cash )->take( 1 )->delete();
+            }
+        }
+        return 0;
+    }
+}
