305461dc95893485dab767cf492b9a9020705c80
Harris Marfel <hrace009@gmail.com>
2022-09-10 10:43:10 +0700

    add news category

--- resources/views/components/hrace009/portal/widget.blade.php
+++ resources/views/components/hrace009/portal/widget.blade.php
@@ -26,11 +26,23 @@
     </div>
     <div class="block-content">
         {{ __('widget.players_online') }}
-        <span class="badge {{ (count($api->getOnlineList()) > 0 ? 'bg-success' : 'bg-danger') }}" style="float: right;">{{ $api->online ? count($api->getOnlineList()) : 0 }}</span>
+        <span class="badge {{ (count($api->getOnlineList()) > 0 ? 'bg-success' : 'bg-danger') }}"
+              style="float: right;">{{ $api->online ? count($api->getOnlineList()) : 0 }}</span>
     </div>
     <div class="block-content">
         {{ __('widget.total_account') }}
-        <span class="badge {{ ($totalUser > 0 ? 'bg-success' : 'bg-danger') }}" style="float: right;">{{ $totalUser }}</span>
+        <span class="badge {{ ($totalUser > 0 ? 'bg-success' : 'bg-danger') }}"
+              style="float: right;">{{ $totalUser }}</span>
     </div>
 </div>
 <!-- /Server Time -->
+<!-- News Category -->
+<div class="side-block">
+    <h4 class="block-title">{{ __('widget.table.category') }}</h4>
+    <ul class="block-content">
+        @foreach( __('news.category') as $key => $value )
+            <a href="{{ route('show.article.by.category', $key ) }}">{{ $value }}</a>
+        @endforeach
+    </ul>
+</div>
+<!-- /News Category -->
