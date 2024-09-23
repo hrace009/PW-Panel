742ae70dd43ae2541ffadb5bad7369557d7b4a62
Harris Marfel <hrace009@gmail.com>
2024-09-23 10:46:07 +0700

    Disable loading external if debug true

--- resources/views/components/hrace009/pixel.blade.php
+++ resources/views/components/hrace009/pixel.blade.php
@@ -1,28 +1,32 @@
-<meta name="facebook-domain-verification" content="r77pjkks08dk5620mn8wk8427y170k"/>
-<!-- Meta Pixel Code -->
-<script>
-    !function (f, b, e, v, n, t, s) {
-        if (f.fbq) return;
-        n = f.fbq = function () {
-            n.callMethod ?
-                n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-        };
-        if (!f._fbq) f._fbq = n;
-        n.push = n;
-        n.loaded = !0;
-        n.version = '2.0';
-        n.queue = [];
-        t = b.createElement(e);
-        t.async = !0;
-        t.src = v;
-        s = b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t, s)
-    }(window, document, 'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1568126676737323');
-    fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-               src="https://www.facebook.com/tr?id=1568126676737323&ev=PageView&noscript=1"
-    /></noscript>
-<!-- End Meta Pixel Code -->
+@if( config('app.debug') === false )
+
+    <meta name="facebook-domain-verification" content="r77pjkks08dk5620mn8wk8427y170k"/>
+    <!-- Meta Pixel Code -->
+    <script>
+        !function (f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function () {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1568126676737323');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=1568126676737323&ev=PageView&noscript=1"
+        /></noscript>
+    <!-- End Meta Pixel Code -->
+
+@endif
--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -51,11 +51,15 @@
                 <!-- /Single News Block -->
             @endforeach
         @else
-            <iframe class="center-block" width="560" height="315"
-                    src="https://www.youtube.com/embed/Z6q_u3EGJsw" title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen></iframe>
+            @if( config('app.debug') === false )
+                <iframe class="center-block" width="560" height="315"
+                        src="https://www.youtube.com/embed/Z6q_u3EGJsw" title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+            @else
+                Welcome
+            @endif
         @endif
         <!-- Pagination -->
         {{ $news->links('vendor.pagination.portal') }}
